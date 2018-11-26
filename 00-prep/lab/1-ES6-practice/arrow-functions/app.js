028ef9962e34f0c27e0a992514ac5817beb4b28b
Joe <pumphrey.joseph@gmail.com>
2018-11-26 14:16:10 -0800

    finished arrow lab

--- 00-prep/lab/1-ES6-practice/arrow-functions/app.js
+++ 00-prep/lab/1-ES6-practice/arrow-functions/app.js
@@ -189,17 +189,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-// 
+//  this refers to the joe object
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// this refers to the global window object
 // 3. Explain why "this" is different when an arrow function is used.
-// 
+//  this is different when used in an arrow function beacause arrow functions 'bubble' up to the next nearest scope. Since we used arrow functions in the file exclusivly its going to attach itself to global window object.
