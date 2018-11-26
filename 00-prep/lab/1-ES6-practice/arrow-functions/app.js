7919a3504006b89dacdb86e97784f25901c86302
Joe <pumphrey.joseph@gmail.com>
2018-11-26 13:36:38 -0800

    refactored Student.greetng

--- 00-prep/lab/1-ES6-practice/arrow-functions/app.js
+++ 00-prep/lab/1-ES6-practice/arrow-functions/app.js
@@ -172,12 +172,13 @@ Student.prototype.greeting = () => `Hi, my name is ${joe.name}`;
 console.log(joe.greeting());
 
 
-Student.courseName = function() {
-  return 'This student is enrolled in Code 301.';
-};
+// Student.courseName = function() {
+//   return 'This student is enrolled in Code 301.';
+// };
 
+Student.courseName = () => `This student is enrolled in Code 301.`;
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
