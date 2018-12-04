6701d297d67b2836d8e06fa0d89b507dea29bae6
bnates <brian.nations@codefellows.com>
2018-12-04 08:57:40 -0800

    updated demo

--- 06-node-express-apis/api-demo/server.js
+++ 06-node-express-apis/api-demo/server.js
@@ -14,10 +14,31 @@ const app = express();
 // application middleware (not that important yet)
 app.use(cors());
 
-app.get('/testroute', function(request, response) {
-  let animal = { type: 'turtle', name: 'tim' };
-  response.json(animal);
+// test route for checking to ensure API works
+// app.get('/testroute', function(request, response) {
+//   let animal = { type: 'turtle', name: 'tim' };
+//   response.json(animal);
+// });
+
+app.get('/location', (request, response) => {
+  console.log('my request object:', request.body);
+  const locationData = searchToLatLong(request.query.data);
+  response.send(locationData);
 });
+
+// helper function goes here
+function searchToLatLong(query) {
+  const geoData = require('./data/geo.json');
+  const location = new Location(geoData.results[0]);
+  location.search_query = query;
+  return location;
+}
+
+function Location(data) {
+  this.formatted_query = data.formatted_address;
+  this.latitude = data.geometry.location.lat;
+  this.longitude = data.geometry.location.lng;
+}
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
